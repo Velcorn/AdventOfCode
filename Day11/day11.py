5f22c75fe30277b93f43c1a66a288c834917d2a3
velcorn <8927053+Velcorn@users.noreply.github.com>
2024-12-12 04:05:14 +0100

    Day 11 Part Two - I cheated just a bit...

--- Day11/day11.py
+++ Day11/day11.py
@@ -1,30 +1,52 @@
 # Read file as list
-with open('example.txt') as f:
+with open('input.txt') as f:
     stones = [stone for stone in f.read().split()]
 
 
-def blink(stones, blinks):
-    for _ in range(blinks):
-        new_stones = []
-        for stone in stones:
-            size = len(stone)
-            if stone == '0':
-                new_stones.append('1')
-            elif size % 2 == 0:
-                half = size // 2
-                left = stone[:half]
-                right = stone[half:].lstrip('0') or '0'
-                new_stones.extend([left, right])
-            else:
-                new_stones.append(str(int(stone) * 2024))
-        stones = new_stones
-    return stones
+def count_stones(stones, blinks):
+    # Use memoization to cache results
+    memo = {}
+
+    def blink(stone, blinks):
+        # Create a unique key for memoization
+        key = (stone, blinks)
+
+        # Check if we've already calculated this state
+        if key in memo:
+            return memo[key]
+
+        # Base case: no more blinks
+        if blinks == 0:
+            return 1
+
+        # If stone is 0, it becomes 1 stone
+        if stone == '0':
+            result = blink('1', blinks - 1)
+            memo[key] = result
+            return result
+
+        # If even number of digits, splits into two stones
+        if len(stone) % 2 == 0:
+            mid = len(stone) // 2
+            left = stone[:mid]
+            right = stone[mid:].lstrip('0') or '0'
+            result = blink(left, blinks - 1) + blink(right, blinks - 1)
+            memo[key] = result
+            return result
+
+        # Odd number of digits: multiply by 2024
+        result = blink(str(int(stone) * 2024), blinks - 1)
+        memo[key] = result
+        return result
+
+    # Sum the stone counts for all initial stones
+    return sum(blink(str(stone), blinks) for stone in stones)
 
 
 # Part One: Get the number of stones after 25 blinks
-one_stones = blink(stones, 25)
-print(f'Part One: {len(one_stones)}')
+one_stones = count_stones(stones, 25)
+print(f'Part One: {one_stones}')
 
 # Part One: Get the number of stones after 75 blinks
-two_stones = blink(stones, 75)
-print(f'Part Two: {len(two_stones)}')
+two_stones = count_stones(stones, 75)
+print(f'Part Two: {two_stones}')
