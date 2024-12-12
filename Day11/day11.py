b7d17657f413ccbbe44aebbeae2c933f07ed4f60
velcorn <8927053+Velcorn@users.noreply.github.com>
2024-12-12 04:16:52 +0100

    Minor text fixes

--- Day11/day11.py
+++ Day11/day11.py
@@ -10,36 +10,32 @@ def count_stones(stones, blinks):
     def blink(stone, blinks):
         # Create a unique key for memoization
         key = (stone, blinks)
-
-        # Check if we've already calculated this state
-        if key in memo:
-            return memo[key]
-
         # Base case: no more blinks
         if blinks == 0:
             return 1
-
-        # If stone is 0, it becomes 1 stone
-        if stone == '0':
+        # Check if we've already calculated this state
+        elif key in memo:
+            return memo[key]
+        # If stone is 0, it is replaced with 1
+        elif stone == '0':
             result = blink('1', blinks - 1)
             memo[key] = result
             return result
-
-        # If even number of digits, splits into two stones
-        if len(stone) % 2 == 0:
+        # If stone has even number of digits, it is split into two stones of equal length
+        # with leading zeros removed from the right stone
+        elif len(stone) % 2 == 0:
             mid = len(stone) // 2
             left = stone[:mid]
             right = stone[mid:].lstrip('0') or '0'
             result = blink(left, blinks - 1) + blink(right, blinks - 1)
             memo[key] = result
             return result
+        # Else new stone is the product of the original stone and 2024
+        else:
+            result = blink(str(int(stone) * 2024), blinks - 1)
+            memo[key] = result
+            return result
 
-        # Odd number of digits: multiply by 2024
-        result = blink(str(int(stone) * 2024), blinks - 1)
-        memo[key] = result
-        return result
-
-    # Sum the stone counts for all initial stones
     return sum(blink(str(stone), blinks) for stone in stones)
 
 
