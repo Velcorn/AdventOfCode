3a9cc58bd6c21430d6cd718f46173e6ae733a136
Velcorn <Velcorn@users.noreply.github.com>
2024-12-13 18:48:43 +0100

    Day 12 Part Two - no idea why unique boundaries is missing some

--- Day12/day12.py
+++ Day12/day12.py
@@ -1,21 +1,35 @@
 # Read input as list of lists
 with open('example.txt') as f:
-    garden_plots = [[char for char in line.strip()] for line in f]
+    plots = [[char for char in line.strip()] for line in f]
 
 
 # For each plant (A-Z), perform a BFS only moving to same type of plant while tracking the perimeter, i.e.,
 # adjacent plants of a different type or out of bounds. The result is the area of the garden plot, i.e., the
-# number of plants visited multiplied by the perimeter.
-def get_fencing_prices(garden_plots, discount):
+# number of plants visited multiplied by the perimeter or sides of the garden plot if discounted.
+def get_fencing_prices(plots, discount):
     visited_plots = set()
+
+    def unique_boundary(node, neighbor):
+        # Add unique boundary, i.e., relevant coordinate of current node (either (0, j) or (i, 0))
+        # and direction of boundary (N, S, W, E)
+        i, j = node
+        x, y = neighbor
+        if x < i:
+            return i, 0, 'N'
+        elif x > i:
+            return i, 0, 'S'
+        elif y < j:
+            return 0, j, 'W'
+        else:
+            return 0, j, 'E'
 
     def bfs(visited, i, j, discount):
         if (i, j) in visited_plots:
             return 0
         else:
             visited.add((i, j))
+        plant_type = plots[i][j]
         visited_plants = set()
-        plant_type = garden_plots[i][j]
         perimeter = 0
         unique_boundaries = set()
         q = [(i, j)]
@@ -25,27 +39,31 @@ def get_fencing_prices(garden_plots, discount):
                 continue
             visited_plots.add((i, j))
             visited_plants.add((i, j))
-            neighbors = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
+            neighbors = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
             for x, y in neighbors:
-                if 0 <= x < len(garden_plots) and 0 <= y < len(garden_plots[0]):
-                    if garden_plots[x][y] == plant_type:
+                if 0 <= x < len(plots) and 0 <= y < len(plots[0]):
+                    if plots[x][y] == plant_type:
                         q.append((x, y))
                     else:
                         perimeter += 1
+                        unique_boundaries.add(unique_boundary((i, j), (x, y)))
                 else:
                     perimeter += 1
+                    unique_boundaries.add(unique_boundary((i, j), (x, y)))
         area = len(visited_plants)
-        sides = len(unique_boundaries)
         if discount:
+            sides = len(unique_boundaries)
+            print(f'Plant: {plant_type}, Area: {area}, Sides: {sides}')
             return area * sides
         return area * perimeter
-    return sum(bfs(visited_plots, i, j, discount) for i in range(len(garden_plots)) for j in range(len(garden_plots[0])))
+    return sum(bfs(visited_plots, i, j, discount) for i in range(len(plots)) for j in range(len(plots[0])))
 
 
 # Part One: Get the total price of fencing for the garden plots
-for row in garden_plots:
-    print(row)
-print(f'Part One: {get_fencing_prices(garden_plots, discount=False)}')
+for row in plots:
+    print(''.join(row))
+
+print(f'Part One: {get_fencing_prices(plots, discount=False)}')
 
 # Part Two: Get the total price of fencing for the garden plots with a discount
-print(f'Part Two: {get_fencing_prices(garden_plots, discount=True)}')
+print(f'Part Two: {get_fencing_prices(plots, discount=True)}')
