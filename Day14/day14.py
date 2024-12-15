4f111f367b352457e27293fc8ea34d68d814ae99
Velcorn <Velcorn@users.noreply.github.com>
2024-12-15 14:57:46 +0100

    Day 14 - ####### best tree detector

--- Day14/day14.py
+++ Day14/day14.py
@@ -1,5 +1,4 @@
 import re
-from math import log2
 
 pattern = re.compile(r'p=(\d+),(\d+) v=(-?\d+),(-?\d+)')
 
@@ -12,55 +11,41 @@ with open(file) as f:
         p1, p2, v1, v2 = pattern.match(line).groups()
         robots[i] = (int(p1), int(p2), int(v1), int(v2))
 
-# Part One: Calculate the safety factor after 100 seconds
-# The safety factor is the number of robots within each quadrant of the grid multiplied by each other
+# Initialize grid with dimensions w x h
 w, h = (11, 7) if file == 'example.txt' else (101, 103)
 grid = [[0 for x in range(w)] for y in range(h)]
+
+# Part One: Calculate the safety factor after 100 seconds
+# The safety factor is the number of robots within each quadrant of the grid multiplied by each other
+grid_ = grid.copy()
 for i in range(len(robots)):
-    x, y, vx, vy = robots.get(i)
+    x, y, vx, vy = robots[i]
     nx, ny = (x + 100 * vx) % w, (y + vy * 100) % h
-    grid[ny][nx] += 1
-q1 = sum(grid[y][x] for y in range(h // 2) for x in range(w // 2))
-q2 = sum(grid[y][x] for y in range(h // 2) for x in range(w // 2 + 1, w))
-q3 = sum(grid[y][x] for y in range(h // 2 + 1, h) for x in range(w // 2))
-q4 = sum(grid[y][x] for y in range(h // 2 + 1, h) for x in range(w // 2 + 1, w))
-print(f'Part One: {q1 * q2 * q3 * q4}')
+    grid_[ny][nx] += 1
+q1 = sum(grid_[y][x] for y in range(h // 2) for x in range(w // 2))
+q2 = sum(grid_[y][x] for y in range(h // 2) for x in range(w // 2 + 1, w))
+q3 = sum(grid_[y][x] for y in range(h // 2 + 1, h) for x in range(w // 2))
+q4 = sum(grid_[y][x] for y in range(h // 2 + 1, h) for x in range(w // 2 + 1, w))
+safety_factor = q1 * q2 * q3 * q4
+print(f'Part One: {safety_factor}')
 
 
 # Part Two: Get the fewest number of seconds that must elapse for the robots to display a christmas tree
-def calculate_entropy(grid):
-    w, h = len(grid[0]), len(grid)
-    total_pixels = w * h
-
-    # Calculate the entropy of the grid
-    ones = sum(1 if grid[y][x] > 0 else 0 for y in range(h) for x in range(w))
-    zeros = total_pixels - ones
-
-    p_ones = ones / total_pixels
-    p_zeros = zeros / total_pixels
-
-    entropy = 0
-    if 0 < p_ones < 1:
-        entropy = -(p_zeros * log2(p_zeros) + p_ones * log2(p_ones))
-
-    return entropy
-
-
-grid = [[0 for x in range(w)] for y in range(h)]
-safety_factor = 0
+grid_ = grid.copy()
 seconds = 0
 while True:
     for i in range(len(robots)):
-        x, y, vx, vy = robots.get(i)
+        x, y, vx, vy = robots[i]
         nx, ny = (x + vx) % w, (y + vy) % h
         robots[i] = (nx, ny, vx, vy)
-        grid[ny][nx] += 1
-        if grid[y][x] > 0:
-            grid[y][x] -= 1
+        grid_[ny][nx] += 1
+        if grid_[y][x] > 0:
+            grid_[y][x] -= 1
     seconds += 1
-    entropy = calculate_entropy(grid)
-    if entropy < 0.27:
-        with open(f'grid_{seconds}.txt', 'w') as f:
-            for row in grid:
-                f.write(''.join(str(x) for x in row) + '\n')
+    row = ''.join(['#' if grid_[y][x] > 0 else '.' for x in range(w) for y in range(h)])
+    # ####### best tree detector: can't be bothered to implement a proper one
+    if '########' in row:
+        with open('output.txt', 'w') as f:
+            f.write('\n'.join([''.join(['#' if grid_[y][x] > 0 else '.' for x in range(w)]) for y in range(h)]))
+        break
 print(f'Part Two: {seconds}')
