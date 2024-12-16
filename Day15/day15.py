e2e0352cec045818492e3ca543c87901d5212349
velcorn <8927053+Velcorn@users.noreply.github.com>
2024-12-16 03:12:12 +0100

    Day 15 Part One done

--- Day15/day15.py
+++ Day15/day15.py
@@ -1,5 +1,5 @@
 # Read input as list of lists and single string of moves
-with open('sexample.txt') as f:
+with open('input.txt') as f:
     lines = f.read().splitlines()
     warehouse = []
     moves = []
@@ -23,31 +23,43 @@ def find_char(lab, char, first=False):
         return indices
 
 
-# Part One: Get the sum of all boxes' GPS coordinates
-directions = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
-robot = find_char(warehouse, '@', first=True)
-for move in moves:
+def move_robot(warehouse, robot, move):
     x, y = robot
     dx, dy = directions[move]
     nx, ny = x + dx, y + dy
     # If next position is free, move the robot
     if warehouse[nx][ny] == '.':
-        warehouse[nx][ny] = '@'
         warehouse[x][y] = '.'
+        warehouse[nx][ny] = '@'
         robot = (nx, ny)
     # If next position is a box, check if the box can be pushed, i.e., there is free space anywhere between the box
-    # and the wall
+    # and the wall and push the box and adjacent boxes if any
     elif warehouse[nx][ny] == 'O':
-        if warehouse[nx + dx][ny + dy] == '.':
-            warehouse[nx + dx][ny + dy] = 'O'
-            warehouse[nx][ny] = '@'
+        new_boxes = []
+        while warehouse[nx][ny] == 'O':
+            new_boxes.append((nx, ny))
+            nx, ny = nx + dx, ny + dy
+        if warehouse[nx][ny] == '.':
+            rx, ry = new_boxes.pop(0)
             warehouse[x][y] = '.'
-            robot = (nx, ny)
-    else:
-        continue
+            warehouse[rx][ry] = '@'
+            warehouse[nx][ny] = 'O'
+            robot = (rx, ry)
+            for box in new_boxes:
+                x, y = box
+                warehouse[x][y] = 'O'
+    return warehouse, robot
 
-for row in warehouse:
-    print(''.join(row))
+
+# Part One: Get the sum of all boxes' GPS coordinates
+directions = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
+robot = find_char(warehouse, '@', first=True)
+for move in moves:
+    warehouse, robot = move_robot(warehouse, robot, move)
+    for row in warehouse:
+        print(''.join(row))
+    print()
+
 
 boxes = find_char(warehouse, 'O')
 box_coords_sum = sum([100 * x + y for x, y in boxes])
