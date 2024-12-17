e701c2f870b14d51ea7ca00c5968dec7b2ebb17f
velcorn <8927053+Velcorn@users.noreply.github.com>
2024-12-17 02:39:30 +0100

    Day 15 Part Two somewhat clean

--- Day15/day15.py
+++ Day15/day15.py
@@ -1,8 +1,7 @@
-from copy import deepcopy
 from time import sleep
 
 # Read input as list of lists and single string of moves
-with open('lexample.txt') as f:
+with open('input.txt') as f:
     lines = f.read().splitlines()
     warehouse = []
     moves = []
@@ -12,6 +11,17 @@ with open('lexample.txt') as f:
         else:
             moves.append(line)
     moves = [c for c in ''.join(moves)]
+
+
+# Set up for part one and two
+directions = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
+scaling = {'#': ['#', '#'], '.': ['.', '.'], 'O': ['[', ']'], '@': ['@', '.']}
+swarehouse = []
+for row in warehouse:
+    srow = []
+    for cell in row:
+        srow.extend(scaling[cell])
+    swarehouse.append(srow)
 
 
 def find_char(lab, char, first=False):
@@ -27,109 +37,127 @@ def find_char(lab, char, first=False):
 
 
 def move_robot(warehouse, robot, move):
-    x, y = robot
+    rx, ry = robot
     dx, dy = directions[move]
-    nx, ny = x + dx, y + dy
+    nx, ny = rx + dx, ry + dy
+
     # If next position is free, move the robot
     if warehouse[nx][ny] == '.':
-        warehouse[x][y] = '.'
+        warehouse[rx][ry] = '.'
         warehouse[nx][ny] = '@'
         robot = (nx, ny)
-    # If next position is a box, check if the box can be pushed, i.e., there is free space anywhere between the box
-    # and the wall and push the box and adjacent boxes if any
+
+    # Handle pushing box for standard boxes
     elif warehouse[nx][ny] == 'O':
         new_boxes = []
         while warehouse[nx][ny] == 'O':
             new_boxes.append((nx, ny))
             nx, ny = nx + dx, ny + dy
+
         if warehouse[nx][ny] == '.':
             rx, ry = new_boxes.pop(0)
-            warehouse[x][y] = '.'
+            warehouse[robot[0]][robot[1]] = '.'
             warehouse[rx][ry] = '@'
             warehouse[nx][ny] = 'O'
             robot = (rx, ry)
+
             for box in new_boxes:
                 x, y = box
                 warehouse[x][y] = 'O'
+
+    # Handle pushing scaled boxes for horizontal moves
+    elif warehouse[nx][ny] in ['[', ']'] and move in ['<', '>']:
+        new_boxes = []
+        while warehouse[nx][ny] in ['[', ']']:
+            new_boxes.append((nx, ny))
+            nx, ny = nx + dx, ny + dy
+
+        if warehouse[nx][ny] == '.':
+            nrx, nry = new_boxes.pop(0)
+            warehouse[rx][ry] = '.'
+            warehouse[nrx][nry] = '@'
+            warehouse[nx][ny] = ']' if move == '>' else '['
+            robot = (nrx, nry)
+
+            for i, box in enumerate(new_boxes):
+                bx, by = box
+                if i % 2 == 0:
+                    warehouse[bx][by] = '[' if move == '>' else ']'
+                else:
+                    warehouse[bx][by] = ']' if move == '>' else '['
+
+    # Handle pushing scaled boxes for vertical moves
+    elif warehouse[nx][ny] in ['[', ']'] and move in ['^', 'v']:
+        # Helper function to get box sides
+        def get_sides(bx, by):
+            return (bx, by - 1, bx, by) if warehouse[bx][by] == ']' else (bx, by, bx, by + 1)
+
+        # Helper function to get connected boxes
+        def get_boxes(box, move):
+            dx, dy = directions[move]
+            lbx, lby, rbx, rby = box
+            boxes = [(lbx, lby, rbx, rby)]
+            visited = set()
+            q = [(lbx, lby, rbx, rby)]
+
+            while q:
+                lbx, lby, rbx, rby = q.pop(0)
+                if (lbx, lby, rbx, rby) in visited:
+                    continue
+
+                boxes.append((lbx, lby, rbx, rby))
+                visited.add((lbx, lby, rbx, rby))
+
+                lbnx, lbny = lbx + dx, lby + dy
+                rbnx, rbny = rbx + dx, rby + dy
+
+                if warehouse[lbnx][lbny] == '#' or warehouse[rbnx][rbny] == '#':
+                    return []
+
+                if warehouse[lbnx][lbny] in ['[', ']']:
+                    q.append(get_sides(lbnx, lbny))
+                if warehouse[rbnx][rbny] in ['[', ']']:
+                    q.append(get_sides(rbnx, rbny))
+
+            return boxes
+
+        # Main vertical box pushing logic
+        lbx, lby, rbx, rby = get_sides(nx, ny)
+        boxes = get_boxes((lbx, lby, rbx, rby), move)
+
+        if boxes:
+            for box in reversed(boxes):
+                lbx, lby, rbx, rby = box
+                lbnx, lbny = lbx + dx, lby + dy
+                rbnx, rbny = rbx + dx, rby + dy
+
+                if warehouse[lbnx][lbny] == '.':
+                    warehouse[lbx][lby] = '.'
+                    warehouse[lbnx][lbny] = '['
+
+                if warehouse[rbnx][rbny] == '.':
+                    warehouse[rbx][rby] = '.'
+                    warehouse[rbnx][rbny] = ']'
+
+            warehouse[rx][ry] = '.'
+            warehouse[nx][ny] = '@'
+            robot = (nx, ny)
+
     return warehouse, robot
 
 
-def smove_robot(swarehouse, robot, move):
-    x, y = robot
-    dx, dy = directions[move]
-    nx, ny = x + dx, y + dy
-    # If next position is free, move the robot
-    if swarehouse[nx][ny] == '.':
-        swarehouse[x][y] = '.'
-        swarehouse[nx][ny] = '@'
-        robot = (nx, ny)
-    # If next position is a box, check if the box can be pushed, i.e., there is free space anywhere between the box
-    # and the wall and push the box and adjacent boxes if any
-    elif swarehouse[nx][ny] in ['[', ']'] and move in ['<', '>']:
-        new_boxes = []
-        while swarehouse[nx][ny] in ['[', ']']:
-            new_boxes.append((nx, ny))
-            nx, ny = nx + dx, ny + dy
-        if swarehouse[nx][ny] == '.':
-            rx, ry = new_boxes.pop(0)
-            swarehouse[x][y] = '.'
-            swarehouse[rx][ry] = '@'
-            swarehouse[nx][ny] = ']' if move == '>' else '['
-            robot = (rx, ry)
-            for i, box in enumerate(new_boxes):
-                x, y = box
-                if i % 2 == 0:
-                    swarehouse[x][y] = '[' if move == '>' else ']'
-                else:
-                    swarehouse[x][y] = ']' if move == '>' else '['
-    elif swarehouse[nx][ny] in ['[', ']'] and move in ['^', 'v']:
-        new_boxes = []
-        bside = swarehouse[nx][ny]
-        # With each step, look at the
-        while swarehouse[nx][ny] in ['[', ']']:
-            new_boxes.append((nx, ny, swarehouse[nx][ny]))
-            nx, ny = nx + dx, ny + dy
-        if swarehouse[nx][ny] == '.':
-            rx, ry, bside = new_boxes.pop(0)
-            swarehouse[x][y] = '.'
-            swarehouse[rx][ry] = '@'
-            swarehouse[nx][ny] = bside
-            robot = (rx, ry)
-            for i, box in enumerate(new_boxes):
-                x, y = box
-                if i % 2 == 0:
-                    swarehouse[x][y] = '[' if move == 'v' else ']'
-                else:
-                    swarehouse[x][y] = ']' if move == 'v' else '['
-    return swarehouse, robot
-
-
 # Part One: Get the sum of all boxes' GPS coordinates
-directions = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
 robot = find_char(warehouse, '@', first=True)
-warehouse_ = deepcopy(warehouse)
 for move in moves:
-    warehouse_, robot = move_robot(warehouse_, robot, move)
-boxes = find_char(warehouse_, 'O')
+    warehouse, robot = move_robot(warehouse, robot, move)
+boxes = find_char(warehouse, 'O')
 box_coords_sum = sum([100 * x + y for x, y in boxes])
 print(f'Part One: {box_coords_sum}')
 
 # Part Two: Get the sum of all boxes' GPS coordinates in scaled warehouse
-scaling = {'#': ['#', '#'], '.': ['.', '.'], 'O': ['[', ']'], '@': ['@', '.']}
-swarehouse = []
-for row in warehouse:
-    srow = []
-    for cell in row:
-        srow.extend(scaling[cell])
-    swarehouse.append(srow)
-for row in swarehouse:
-    print(''.join(row))
-print()
 robot = find_char(swarehouse, '@', first=True)
-swarehouse_ = deepcopy(swarehouse)
 for move in moves:
-    swarehouse_, robot = smove_robot(swarehouse_, robot, move)
-    for row in swarehouse_:
-        print(''.join(row))
-    print(move)
-    sleep(1)
+    swarehouse, robot = move_robot(swarehouse, robot, move)
+boxes = find_char(swarehouse, '[')
+box_coords_sum = sum([100 * x + y for x, y in boxes])
+print(f'Part Two: {box_coords_sum}')
