86c83692e7b5cfe8f013db8c8f8bc02db031802d
velcorn <8927053+Velcorn@users.noreply.github.com>
2024-12-29 03:48:27 +0100

    D24P2: Should probably just build a graph and visualize - will have to come back later

--- 2024/24/24.py
+++ 2024/24/24.py
@@ -12,41 +12,24 @@ with open('24_input.txt') as f:
             wire_conns.append([i1, i2, gate, o])
 
 
-def get_decimal_from_prefix(wires, prefix):
-    filtered = {w: v for w, v in wires.items() if w.startswith(prefix)}
-    binary = ''.join(str(filtered[w]) for w in sorted(filtered, reverse=True))
-    return binary, int(binary, 2)
-
-
-# Simulate the circuit, tracking the connections of all y wires from input to output
-yres = [[w] for w in wire_values if w.startswith('y')]
+# Simulate the circuit
+wire_conns = sorted(wire_conns, reverse=True)
 while wire_conns:
     for wc in wire_conns:
         i1, i2, gate, o = wc
-        if all(i in wire_values for i in [i1, i2]):
+        if i1 in wire_values and i2 in wire_values:
             if gate == 'AND':
                 wire_values[o] = wire_values[i1] & wire_values[i2]
             elif gate == 'OR':
                 wire_values[o] = wire_values[i1] | wire_values[i2]
             else:
                 wire_values[o] = wire_values[i1] ^ wire_values[i2]
-            for w in yres:
-                if i1 in w[-1]:
-                    w[-1] = f'{i1}:{wire_values[i1]}'
-                    w.append(f'{i2}:{wire_values[i2]}')
-                    w.append(gate)
-                    w.append(f'{o}:{wire_values[o]}')
-                elif i2 == w[-1]:
-                    w[-1] = f'{i2}:{wire_values[i2]}'
-                    w.append(f'{i1}:{wire_values[i1]}')
-                    w.append(gate)
-                    w.append(f'{o}:{wire_values[o]}')
             wire_conns.remove(wc)
 
 # Part One: The decimal number that the wires starting with z output
-z_binary, z_decimal = get_decimal_from_prefix(wire_values, 'z')
+z_wires = {w: v for w, v in wire_values.items() if w.startswith('z')}
+z_binary = ''.join(str(z_wires[w]) for w in sorted(z_wires, reverse=True))
+z_decimal = int(z_binary, 2)
 print(f'Part One: {z_decimal}')
 
-
 # Part Two: The eight wires involved in a swap sorted alphabetically and joined with commas
-
