f5b04004bda251d0d28f0c28b3599fdb05b98b4f
velcorn <8927053+Velcorn@users.noreply.github.com>
2025-01-01 03:58:32 +0100

    D24P2: Can't be bothered to put more time into this when the manual check with some domain knowledge is the easiest and most straightforward solution

--- 2024/24/24.py
+++ 2024/24/24.py
@@ -1,6 +1,32 @@
 import networkx as nx
 from collections import deque
 from graphviz import Digraph
+from itertools import combinations
+
+
+def simulate_circuit(wvs, wcs, bits=None):
+    queue = deque(wcs)
+    z_wires = {}
+    while queue:
+        i1, gate, i2, o = queue.popleft()
+        # If both input wires have values, calculate the output wire value
+        if i1 in wvs and i2 in wvs:
+            if gate == 'AND':
+                wvs[o] = wvs[i1] & wvs[i2]
+            elif gate == 'OR':
+                wvs[o] = wvs[i1] | wvs[i2]
+            elif gate == 'XOR':
+                wvs[o] = wvs[i1] ^ wvs[i2]
+            if o.startswith('z'):
+                z_wires[o] = wvs[o]
+        # Otherwise, add the connection back to the queue
+        else:
+            queue.append((i1, gate, i2, o))
+        if bits and len(z_wires) == bits:
+            break
+    z_binary = ''.join(str(z_wires[w]) for w in sorted(z_wires, reverse=True))
+    return z_binary, wvs, wcs
+
 
 # Read input as dict for initial wire values and list for wire connections
 with open('24_input.txt') as f:
@@ -21,30 +47,13 @@ for i1, gate, i2, o in wire_conns:
     graph.add_edge(i1, o, gate=gate)
     graph.add_edge(i2, o, gate=gate)
 
-# Simulate the circuit and track depths
-queue = deque(wire_conns)
-while queue:
-    i1, gate, i2, o = queue.popleft()
-    # If both input wires have values, calculate the output wire value
-    if i1 in wire_values and i2 in wire_values:
-        if gate == 'AND':
-            wire_values[o] = wire_values[i1] & wire_values[i2]
-        elif gate == 'OR':
-            wire_values[o] = wire_values[i1] | wire_values[i2]
-        elif gate == 'XOR':
-            wire_values[o] = wire_values[i1] ^ wire_values[i2]
-    # Otherwise, add the connection back to the queue
-    else:
-        queue.append((i1, gate, i2, o))
-
 # Part One: The decimal number that the wires starting with 'z' output
-z_wires = {w: v for w, v in wire_values.items() if w.startswith('z')}
-z_binary = ''.join(str(z_wires[w]) for w in sorted(z_wires, reverse=True))
+z_binary = simulate_circuit(wire_values.copy(), wire_conns)[0]
 z_decimal = int(z_binary, 2)
 print(f'Part One: {z_decimal}')
 
-# Part Two: Identify swapped wires based on depth mismatches
-# Just plot the graph for manual inspection - might programmatically solve it later
+# Part Two: The names of all eight swapped wires sorted alphabetically and joined with commas
+# Calculate x + y and find the bits that differ from z to narrow down the adders with swapped wires
 x_wires = {w: v for w, v in wire_values.items() if w.startswith('x')}
 x_binary = ''.join(str(x_wires[w]) for w in sorted(x_wires, reverse=True))
 x_decimal = int(x_binary, 2)
@@ -53,9 +62,10 @@ y_binary = ''.join(str(y_wires[w]) for w in sorted(y_wires, reverse=True))
 y_decimal = int(y_binary, 2)
 xy_decimal = x_decimal + y_decimal
 xy_binary = bin(xy_decimal)[2:]
-# Print the indices of the mismatched bits
 diff = [i for i, (z, xy) in enumerate(zip(reversed(xy_binary), reversed(z_binary))) if z != xy]
 print(diff)
+
+# Visualize the circuit
 dot = Digraph(format="png")
 for node in graph.nodes:
     dot.node(node)
@@ -70,4 +80,4 @@ for edge in graph.edges(data=True):
     else:
         color = 'black'
     dot.edge(edge[0], edge[1], color=color)
-dot.render("24_circuit", view=False)
+dot.render("24_circuit", view=True)
